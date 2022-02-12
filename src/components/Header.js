d568da4bea934dd106fcee9a89954d79dd6a0dc1
ashuxldr <ashuxldr@gmail.com>
2022-02-12 23:04:12 +0530

    header, sidepanel done

---
+++ src/components/Header.js
@@ -0,0 +1,114 @@
+import React from 'react'
+import styled from 'styled-components'
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
+function Header() {
+  return (
+    <Wrapper>
+        <LogoWrapper>
+            <Logo><MenuIcon/></Logo>
+            <Text>E-Envelope</Text>
+        </LogoWrapper>
+        <SearchWrapper>
+            <SearchBarWrapper>
+                <SearchIcon/>
+                <input placeholder='Search envelopes'/>
+            </SearchBarWrapper>
+        </SearchWrapper>
+        <UserWrapper>
+            <AddressID>UsernameEE.com</AddressID>
+            <MenuIcon/>
+        </UserWrapper>
+    </Wrapper>
+  )
+}
+
+export default Header
+
+const Wrapper = styled.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:space-between;
+    align-items:center;    
+    height:70px;
+    width:100%;
+    border-bottom:1px solid lightgray;
+`
+const LogoWrapper = styled.div`
+    display:flex;    
+
+    .MuiSvgIcon-root{
+        margin:0px 20px;
+    }
+    
+`
+const Logo = styled.div`
+    display:grid;    
+`
+// const MenuIcon = styled.div`
+//     display:grid;    
+// `
+const Text = styled.div`
+    color: darkgrey;
+    font-weight:600;
+    font-size:1.5rem;
+`
+const SearchWrapper = styled.div`
+    flex-grow:2;
+    display:flex;  
+    
+`
+
+
+const SearchBarWrapper = styled.div`
+    display:flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    margin: 0px 30px;
+    padding:5px;
+    background-color:lightgray;
+    border-radius:5px;
+
+    .MuiSvgIcon-root{
+        margin-right:20px;
+        margin-left:20px;
+        cursor:pointer;
+    }
+
+    input{
+        width:100%;
+        height:30px;
+        background:none;
+        padding:5px;
+        border:none;
+        font-size:18px;
+        :focus{
+            outline:none;
+        }
+    }
+`
+// const SearchIconW = styled.div`
+    // display:grid;    
+// `
+
+
+const UserWrapper = styled.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:start;
+    align-items:center;
+    .MuiSvgIcon-root{
+        margin-right:20px;
+        margin-left:20px;
+    }
+    
+`
+
+
+const AddressID = styled.div`
+    color:darkgray;
+    font-size:1rem;   
+`
---
+++ src/components/SidePanel.js
@@ -0,0 +1,79 @@
+import Search from '@material-ui/icons/Search'
+import React from 'react'
+import styled from 'styled-components'
+import sidebarItems from '../data/sidebarItems'
+import CreateIcon from '@material-ui/icons/Create';
+
+function SidePanel() {
+  return (
+    <Wrapper>
+        <ComposeLetter>
+            <CreateIcon/>
+            Compose 
+        </ComposeLetter>
+        <SidebarButtonWrapper>
+            {sidebarItems.map(item => (
+                <SidebarButtonItem>{item.icon} {item.text}</SidebarButtonItem>
+            ))}
+        </SidebarButtonWrapper>
+
+    </Wrapper>
+  )
+}
+
+export default SidePanel
+
+const Wrapper = styled.div`
+    height:  calc( 100vh - 70px );
+    display:flex;
+    flex-direction:column;
+    width:20%;
+    background-color:lightgray;
+    border-left:1px solid darkgray;
+    `
+
+const ComposeLetter = styled.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:center;
+    align-items:center;
+    margin:10px 10px;
+    font-size:18px;
+    padding:5px 10px;
+    border-radius:0.5rem;
+    .MuiSvgIcon-root{
+        margin:0px 10px;
+    }
+    :hover{
+        font-size:20px;
+        background-color:white;
+    }
+`
+
+const SidebarButtonWrapper = styled.div`
+display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+`
+
+const SidebarButtonItem = styled.div`
+    // display:grid;
+    // grid-template-columns: 50% auto;
+    // place-items:center;
+    display:flex;
+    flex-direction:row;
+    
+    margin:10px 10px;
+    font-size:18px;
+    border-radius:0.5rem;
+    background-color:lightgray;
+    padding:5px 10px;
+    .MuiSvgIcon-root{
+        margin:0px 10px;
+    }
+    :hover{
+        background-color:white;
+    }
+`
+
